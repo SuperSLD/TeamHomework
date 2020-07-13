39fb740914ba3d03270d1bc803db71fd06fdc5e1
tambov_nikita <tambovnikita@yandex.ru>
2020-07-13 23:48:23 +0300

    Проверка введённых логина и пароля

--- Example_Qt/mainwindow.cpp
+++ Example_Qt/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +14,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString login = ui->lineEdit->text();
+    QString password = ui->lineEdit_2->text();
+
+    if (login == "tambovnikita@yandex.ru" && password == "12345")
+    {
+        QMessageBox::information(this, "Результат авторизации", "Авторизация успешно пройдена!");
+    }
+    else
+    {
+        QMessageBox::warning(this, "Результат авторизации", "Авторизация не пройдена!");
+    }
+}
