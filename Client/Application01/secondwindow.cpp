122ef9bf571430f29faa44e31f3ddf1e13aca7a9
Flyforest <68278911+Flyforest@users.noreply.github.com>
2020-07-17 15:00:12 +0300

    Установлен фон на окно авторизации.

--- Client/Application01/secondwindow.cpp
+++ Client/Application01/secondwindow.cpp
@@ -1,6 +1,15 @@
 #include "secondwindow.h"
 #include "ui_secondwindow.h"
 
+/**
+ * @brief SecondWindow::SecondWindow
+ * @param parent
+ *
+ * Создание окна главного меню. Выпонляется при успешной авторизации.
+ *
+ * @author Zinyukov Pavel (FlyForest962@yandex.ru)
+ */
+
 SecondWindow::SecondWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::SecondWindow)
@@ -18,20 +27,52 @@ SecondWindow::~SecondWindow()
     delete ui;
 }
 
+/**
+ * @brief SecondWindow::on_pushButton_2_clicked
+ *
+ * Выполняется при нажатии кнопки Выход.
+ *
+ * @author Zinyukov Pavel (FlyForest962@yandex.ru)
+ */
+
 void SecondWindow::on_pushButton_2_clicked()
 {
     QApplication::quit();
 }
+
+/**
+ * @brief SecondWindow::on_pushButton_clicked
+ *
+ * Выполняется при нажатии кнопки Файлы.
+ *
+ * @author Zinyukov Pavel (FlyForest962@yandex.ru)
+ */
 
 void SecondWindow::on_pushButton_clicked()
 {
     ui->label_4->setText("Здесь скоро появятся Файлы");
 }
 
+/**
+ * @brief SecondWindow::on_pushButton_4_clicked
+ *
+ * Выполняется при нажатии кнопки Сообщения.
+ *
+ * @author Zinyukov Pavel (FlyForest962@yandex.ru)
+ */
+
 void SecondWindow::on_pushButton_4_clicked()
 {
     ui->label_4->setText("Здесь скоро появятся Сообщения");
 }
+
+/**
+ * @brief SecondWindow::on_pushButton_3_clicked
+ *
+ * Выполняется при нажатии кнопки Задачи.
+ *
+ * @author Zinyukov Pavel (FlyForest962@yandex.ru)
+ */
 
 void SecondWindow::on_pushButton_3_clicked()
 {
